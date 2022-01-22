402dcc682c03bb1776ea6ee1473541e43c18ae20
ktjybl <64632527+ktjybl@users.noreply.github.com>
2022-01-22 13:13:54 +0200

    Add files via upload

---
+++ scripts/scroll.js
@@ -0,0 +1,40 @@
+﻿const scrollFunc=()=>{
+const links = document.querySelectorAll('.header-menu__item a');
+const allCharater = document.querySelector('.card-details__link-characteristics');
+seamless.polyfill();
+const newArr=[...links, allCharater]
+
+newArr.forEach(elem=>{
+    elem.addEventListener('click', (evt)=>{
+       evt.preventDefault();
+
+       const id=elem.getAttribute('href').substring(1);
+       const section = document.getElementById(id);
+       if(section){
+          seamless.elementScrollIntoView(section,{
+               behavior:'smooth',
+               block:'start'
+          });
+       }else{
+          seamless.elementScrollIntoView(document.querySelector('#characteristics'),{
+               behavior:'smooth',
+               block:'start',
+               inline:'center' 
+               }); 
+           }
+    });
+
+allCharater.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const allCharaterId = allCharater.getAttribute('href').substring(1);
+    const allCharaterSection = document.getElementById(allCharaterId);
+
+    seamless.elementScrollIntoView(allCharaterSection, {
+        behavior: "smooth",
+        block: "start"
+    })
+})
+});
+}
+scrollFunc();
---
+++ scripts/tabs.js
@@ -0,0 +1,55 @@
+﻿const tabsFunc=()=>{
+const tabs=document.querySelectorAll('.card-detail__change');
+const tabsTitle=document.querySelectorAll('.card-details__title');
+const tabsPrice=document.querySelectorAll('.card-details__price');
+const tabsImage=document.querySelectorAll('.card__image_item');
+
+const tabsOptions=[
+    {
+       name:"Graphite",
+       memory:'128',
+       price:60000,
+       image:'img/iPhone-graphite.webp'
+    },
+     {
+       name:"Silver",
+       memory:'256',
+       price:65000,
+       image:'img/iPhone-silver.webp'
+    },
+     {
+       name:"Sierra Blue",
+       memory:'512',
+       price:70000,
+       image:'img/iPhone-sierra_blue.webp'
+    }
+];
+
+const changeContent=(index)=>{
+     tabsTitle.innerHTML=`Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
+     tabsPrice.innerHTML=`${tabsOptions[index].price}₽`;
+     tabsImage.setAttribute('src', tabsOptions[index].image);
+      document.title = `iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
+}
+
+const changeActiveTabs=(indexClickedTab)=>{
+     tabs.forEach((tab, index)=>{
+        tab.classList.remove('active');
+
+        if(index===indexClickedTab){
+            tab.classList.add('active');
+         }
+     });
+
+     changeContent(indexClickedTab);
+};
+
+tabs.forEach((tab, index)=>{
+    tab.addEventListener('click', ()=>{
+        changeActiveTabs(index);
+    });
+});
+
+changeContent(0);
+}
+tabsFunc();
